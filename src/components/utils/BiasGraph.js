d0b9c6b4c5c26541f3cadb587e945175e45c3def
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-03-16 20:06:58 +0200

    fix: make bias graph look beautiful

--- src/components/utils/BiasGraph.js
+++ src/components/utils/BiasGraph.js
@@ -16,19 +16,22 @@ const useStyles = makeStyles({
 });
 export const BiasGraph = ({biasInf, biasA, biasB}) => {
     const localClasses = useStyles();
-    let path = `M 0 ${svgSize}`;
-    const biasATranslated = translateBiasA(biasA);
-    const biasBTranslated = translateBiasB(biasB);
-    {
+    const [path, setPath] = React.useState(`M 0 ${svgSize}`);
+
+    React.useEffect(() => {
+        let internalPath = `M 0 ${svgSize}`;
+        const biasATranslated = translateBiasA(biasA);
+        const biasBTranslated = translateBiasB(biasB);
         new Array(numberOfCircles).fill(undefined).forEach((value, index) => {
             const x = 1 / (numberOfCircles - 1) * index;
             const y = biasTanhFunction(x, biasInf, biasATranslated, biasBTranslated);
             const svgX = x * svgSize;
             const svgY = svgSize - y * svgSize;
-            path = path + `L ${svgX} ${svgY}`;
+            internalPath = internalPath + `L ${svgX} ${svgY}`;
         });
-    }
-    console.log('hi');
+        setPath(internalPath);
+    }, [biasInf, biasA, biasB]);
+
     return (
         <svg className={localClasses.svg} width={svgSize} height={svgSize}>
             <path d={path} fill="none" stroke="blueviolet"></path>
