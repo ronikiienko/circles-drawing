ca241889516e4d687cddd05e26f13e821b4e7048
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-04-03 12:27:39 +0300

    wip: use long manual function for getLayerSettings again

--- src/utils/presetUtils.js
+++ src/utils/presetUtils.js
@@ -2,34 +2,91 @@ import {defaultAppSettings, layerPresets, randomPresetRules} from '../consts/con
 import {deepCopy, getBiasedRandomNumber, getRandomHsl, rgbToHex, typeofWithArray} from './generalUtils';
 
 
-const mergeWithDefault = (myValue, defaults) => {
-    const merged = {};
-
-    const merge = (obj, def, current = merged) => {
-        for (let key in def) {
-            // TODO !!!!!!!!!!!!!!!!!!!!!!!! my app uses array to save custom shape.. But if i will make array default too, i will lose items that have bigger index that last default array item..
-            if (!Object.prototype.hasOwnProperty.call(obj, key)) {
-                current[key] = deepCopy(def[key]);
-            } else if (typeof def[key] === 'object') {
-                if (Array.isArray(def[key])) {
-                    current[key] = obj[key];
-                } else {
-                    current[key] = {};
-                    merge(obj[key], def[key], current[key]);
-                }
-            } else {
-                current[key] = obj[key];
-            }
-        }
+export const getLayerSettings = (preset) => {
+    const defaultPreset = layerPresets[0];
+    return {
+        preset: {
+            id: preset?.preset?.id ?? defaultPreset.preset.id,
+            name: preset?.preset?.name ?? defaultPreset.preset.name,
+            description: preset?.preset?.description ?? defaultPreset.preset.description,
+        },
+        size: {
+            size: preset?.size?.size ?? defaultPreset.size.size,
+            sizeRand: preset?.size?.sizeRand ?? defaultPreset.size.sizeRand,
+        },
+        number: {
+            number: preset?.number?.number ?? defaultPreset.number.number,
+        },
+        shape: {
+            shape: preset?.shape?.shape ?? defaultPreset.shape.shape,
+            angle: preset?.shape?.angle ?? defaultPreset.shape.angle,
+            angleRand: preset?.shape?.angleRand ?? defaultPreset.shape.angleRand,
+            widthRatio: preset?.shape?.widthRatio ?? defaultPreset.shape.widthRatio,
+            lineRounded: preset?.shape?.lineRounded ?? defaultPreset.shape.lineRounded,
+            widthRatioRand: preset?.shape?.widthRatioRand ?? defaultPreset.shape.widthRatioRand,
+            lookToOn: preset?.shape?.lookToOn ?? defaultPreset.shape.lookToOn,
+            lookToX: preset?.shape?.lookToX ?? defaultPreset.shape.lookToX,
+            lookToY: preset?.shape?.lookToY ?? defaultPreset.shape.lookToY,
+            rectRoundness: preset?.shape?.rectRoundness ?? defaultPreset.shape.rectRoundness,
+            rectRoundnessRand: preset?.shape?.rectRoundnessRand ?? defaultPreset.shape.rectRoundnessRand,
+            customShape: deepCopy(preset?.shape?.customShape) ?? deepCopy(defaultPreset.shape.customShape),
+            strokeOn: preset?.shape?.strokeOn ?? defaultPreset.shape.strokeOn,
+            strokeThickness: preset?.shape?.strokeThickness ?? defaultPreset.shape.strokeThickness,
+            fillOn: preset?.shape?.fillOn ?? defaultPreset.shape.fillOn,
+        },
+        position: {
+            startX: preset?.position?.startX ?? defaultPreset.position.startX,
+            startY: preset?.position?.startY ?? defaultPreset.position.startY,
+            endX: preset?.position?.endX ?? defaultPreset.position.endX,
+            endY: preset?.position?.endY ?? defaultPreset.position.endY,
+            biasType: preset?.position?.biasType ?? defaultPreset.position.biasType,
+            biasSpiralType: preset?.position?.biasSpiralType ?? defaultPreset.position.biasSpiralType,
+            biasSpiralCustom: preset?.position?.biasSpiralCustom ?? defaultPreset.position.biasSpiralCustom,
+            biasSpiralThickness: preset?.position?.biasSpiralThickness ?? defaultPreset.position.biasSpiralThickness,
+            biasSpiralDensity: preset?.position?.biasSpiralDensity ?? defaultPreset.position.biasSpiralDensity,
+            biasSpiralSpread: preset?.position?.biasSpiralSpread ?? defaultPreset.position.biasSpiralSpread,
+            biasSpiralAngleRand: preset?.position?.biasSpiralAngleRand ?? defaultPreset.position.biasSpiralAngleRand,
+            biasSpiralMult: preset?.position?.biasSpiralMult ?? defaultPreset.position.biasSpiralMult,
+            biasRadiusX: preset?.position?.biasRadiusX ?? defaultPreset.position.biasRadiusX,
+            biasRadiusY: preset?.position?.biasRadiusY ?? defaultPreset.position.biasRadiusY,
+            biasX: preset?.position?.biasX ?? defaultPreset.position.biasX,
+            biasY: preset?.position?.biasY ?? defaultPreset.position.biasY,
+            biasA: preset?.position?.biasA ?? defaultPreset.position.biasA,
+            biasB: preset?.position?.biasB ?? defaultPreset.position.biasB,
+            biasInf: preset?.position?.biasInf ?? defaultPreset.position.biasInf,
+            biasRectXOn: preset?.position?.biasRectXOn ?? defaultPreset.position.biasRectXOn,
+            biasRectYOn: preset?.position?.biasRectYOn ?? defaultPreset.position.biasRectYOn,
+        },
+        color: {
+            color: preset?.color?.color ?? defaultPreset.color.color,
+            strokeColor: preset?.color?.strokeColor ?? defaultPreset.color.strokeColor,
+            colorRand: preset?.color?.colorRand ?? defaultPreset.color.colorRand,
+            glow: preset?.color?.glow ?? defaultPreset.color.glow,
+            transp: preset?.color?.transp ?? defaultPreset.color.transp,
+            transpRand: preset?.color?.transpRand ?? defaultPreset.color.transpRand,
+            overlayMode: preset?.color?.overlayMode ?? defaultPreset.color.overlayMode,
+            blurOn: preset?.color?.blurOn ?? defaultPreset.color.blurOn,
+            blur: preset?.color?.blur ?? defaultPreset.color.blur,
+            blurRand: preset?.color?.blurRand ?? defaultPreset.color.blurRand,
+        },
+        brush: {
+            brushDensity: preset?.brush?.brushDensity ?? defaultPreset.brush.brushDensity,
+            brushOn: preset?.brush?.brushOn ?? defaultPreset.brush.brushOn,
+            brushX: preset?.brush?.brushX ?? defaultPreset.brush.brushX,
+            brushY: preset?.brush?.brushY ?? defaultPreset.brush.brushY,
+        },
     };
-
-    merge(myValue, defaults);
-
-    return merged;
 };
-
-export const getLayerSettings = (preset) => mergeWithDefault(preset, layerPresets[0]);
-export const getAppSettings = (settings) => mergeWithDefault(settings, defaultAppSettings);
+export const getAppSettings = (settings) => {
+    const defaultSettings = defaultAppSettings;
+    return {
+        projectNameRand: settings?.projectNameRand ?? defaultSettings.projectNameRand,
+        projectName: settings?.projectName ?? defaultSettings.projectName,
+        drawingSpeed: settings?.drawingSpeed ?? defaultSettings.drawingSpeed,
+        darkMode: settings?.darkMode ?? defaultSettings.darkMode,
+        resolutionMult: settings?.resolutionMult ?? defaultSettings.resolutionMult,
+    };
+};
 
 export const getRandomPreset = () => {
     const getRandomValue = (valueRule) => {
