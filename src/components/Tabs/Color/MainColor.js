9a739bbdf7c18f4e4f15b0df7edc0c623ab75610
Rostyslav Onikiienko <ronikiienko@gmail.com>
2023-04-07 13:05:08 +0300

    feat: style color subsettings blocks

--- src/components/Tabs/Color/MainColor.js
+++ src/components/Tabs/Color/MainColor.js
@@ -1,4 +1,4 @@
-import {Divider} from '@fluentui/react-components';
+import {makeStyles, shorthands, tokens} from '@fluentui/react-components';
 import React from 'react';
 import {Blur} from './Blur';
 import {Color} from './Color';
@@ -7,18 +7,34 @@ import {Overlay} from './Overlay';
 import {Transp} from './Transp';
 
 
+const useStyles = makeStyles({
+    block: {
+        ...shorthands.padding('8px'),
+        marginBlock: '5px',
+        ...shorthands.borderRadius(tokens.borderRadiusMedium),
+        backgroundColor: tokens.colorNeutralStencil1Alpha,
+    },
+});
+
 export const MainColor = ({settings, handleChange, classes}) => {
+    const localStyles = useStyles();
     return (
         <>
-            <Color settings={settings} handleChange={handleChange} classes={classes}/>
-            <Divider/>
-            <Transp settings={settings} handleChange={handleChange} classes={classes}/>
-            <Divider/>
-            <Glow settings={settings} handleChange={handleChange} classes={classes}/>
-            <Divider/>
-            <Overlay settings={settings} handleChange={handleChange} classes={classes}/>
-            <Divider/>
-            <Blur settings={settings} handleChange={handleChange} classes={classes}/>
+            <div className={localStyles.block}>
+                <Color settings={settings} handleChange={handleChange} classes={classes}/>
+            </div>
+            <div className={localStyles.block}>
+                <Transp settings={settings} handleChange={handleChange} classes={classes}/>
+            </div>
+            <div className={localStyles.block}>
+                <Glow settings={settings} handleChange={handleChange} classes={classes}/>
+            </div>
+            <div className={localStyles.block}>
+                <Overlay settings={settings} handleChange={handleChange} classes={classes}/>
+            </div>
+            <div className={localStyles.block}>
+                <Blur settings={settings} handleChange={handleChange} classes={classes}/>
+            </div>
         </>
     );
 };
